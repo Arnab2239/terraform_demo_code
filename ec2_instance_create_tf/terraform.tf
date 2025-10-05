a419d5e836ee050848e6d043dbaf5727a668f37d
Arnab2239 <155752433+Arnab2239@users.noreply.github.com>
2025-10-05 23:09:42 +0530

    Update terraform.tf

--- ec2_instance_create_tf/terraform.tf
+++ ec2_instance_create_tf/terraform.tf
@@ -5,4 +5,11 @@ terraform {
       # version = "~> 3.0"   # optionally pin provider version
     }
   }
+ backend "s3" {
+    bucket = "arnab-lock-bucket" 
+    key = "terraform.tfstate"
+    region = "us-west-2"
+    dynamodb_table = "arnab-state-table"
+  }
+
 }
